5e6c1f89bd53fbbc576d1fda0ebbbb85bea9ab90
tigerqye <bobcorpisepic@gmail.com>
2023-05-20 21:42:02 -0400

    changed the unit spawning system
    pawn_initial

--- Assets/Scripts/obstacle.cs
+++ Assets/Scripts/obstacle.cs
@@ -5,17 +5,18 @@ using UnityEngine;
 public class obstacle : MonoBehaviour
 {
     private GameObject player;
+    public float speed;
     // Start is called before the first frame update
     void Start()
     {
-        //player = GameObject.FindGameObjectWithTag("Player");
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        this.transform.position -= new Vector3(0, Time.deltaTime, 0);
-        if (this.transform.position.y < -10)
+        this.transform.position -= new Vector3(0, Time.deltaTime*speed, 0);
+        if (this.transform.position.y < -6)
         {
             Destroy(this.gameObject);
         }
@@ -24,6 +25,9 @@ public class obstacle : MonoBehaviour
     void OnTriggerEnter2D(Collider2D OtherObject)
     {
         Debug.Log("COLLISION");
+        // if(OtherObject == player)
+        // {
         Destroy(OtherObject.gameObject);
+        // }
     }
 }
