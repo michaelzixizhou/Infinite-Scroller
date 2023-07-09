52ff30f2b97f7f1c11a01b394c511c649d838a68
michaelzixizhou <106829824+michaelzixizhou@users.noreply.github.com>
2023-07-09 17:35:07 -0400

    Renamed Obstacle

--- Assets/Scripts/ObstacleScripts/Coins.cs
+++ Assets/Scripts/ObstacleScripts/Coins.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Coins : obstacle
+public class Coins : Obstacle
 {
     // Start is called before the first frame update
     void Start()
--- Assets/Scripts/ObstacleScripts/Enemy.cs
+++ Assets/Scripts/ObstacleScripts/Enemy.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : obstacle
+public class Enemy : Obstacle
 {
     private GameObject player;
 
